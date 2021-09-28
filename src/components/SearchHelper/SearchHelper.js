17042fb1776c95a640969afeb149d386f9c2679a
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-09-28 22:23:19 +0300

    search box

--- src/components/SearchHelper/SearchHelper.js
+++ src/components/SearchHelper/SearchHelper.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { findObjects } from "../../http/objectsAPI";
 import { SearchBar } from "..";
+import UserRow from "../UserRow/UserRow";
+import { Link } from "react-router-dom";
+import "./SearchHelper.scss";
 function SearchHelper() {
   const [objects, setObjects] = useState([]);
   const findHandle = async (e) => {
     try {
-      if (e.target.value === "") {
+      if (e.target.value.trim() === "") {
         setObjects([]);
         return;
       }
@@ -19,12 +22,17 @@ function SearchHelper() {
   return (
     <div className="search-helper">
       <SearchBar onChange={findHandle} placeholder="Поиск" />
-      <div className="search-helper__box">
-        {objects &&
-          objects.map((obj) => {
-            return <p>{obj.name + " " + obj.lastName}</p>;
-          })}
-      </div>
+      {objects.length > 0 && (
+        <div className="search-helper__box">
+          {objects &&
+            objects.map((obj) => {
+              console.log(obj);
+              return (
+                <UserRow path={obj.link} name={`${obj.name} ${obj.lastName}`} />
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/UserRow/UserRow.jsx
+++ src/components/UserRow/UserRow.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./UserRow.scss";
 import PersonIcon from "../../assets/img/PersonIcon.png";
+import { Link } from "react-router-dom";
 
-function UserRow({ name, className }) {
+function UserRow({ name, className, path }) {
   return (
-    <a href="#" className={"user-row " + (className ? className : "")}>
+    <Link
+      to={{ pathname: path ? path : "", state: { type: "user" } }}
+      className={"user-row " + (className ? className : "")}
+    >
       <div href="" className="user-row__left">
         <img src={PersonIcon} alt="" className="user-row__img" />
         <p className="user-row__name">{name}</p>
       </div>
       <button className="user-row__btn">Добавить</button>
-    </a>
+    </Link>
   );
 }
 
