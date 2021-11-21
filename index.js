9c60392707e354d669a6222269c87e742c6f6571
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-11-22 00:29:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  let indexUser;
+  console.log("add user");
+  users.forEach((user, index) => {
+    if (user.userId == userId) {
+      indexUser = index;
+    }
+  });
+  const userObject = { userId, socketId };
+  if (indexUser) {
+    users[index] = userObject;
+  }
+  users.push(userObject);
+  console.log(users);
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  const onlineUser = users.find((user) => user.userId == userId);
+  return onlineUser;
+};
+
+io.on("connection", (socket) => {
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ user, conversationUser, text }) => {
+    const userCurr = getUser(conversationUser);
+    if (!userCurr) {
+      return;
+    }
+    io.to(userCurr.socketId).emit("getMessage", {
+      sender: user,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    console.log(users);
+  });
+});
