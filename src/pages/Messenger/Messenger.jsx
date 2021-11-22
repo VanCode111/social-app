de8a486db72cd156c38187324eca02b679aa5a9a
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-11-22 04:36:52 +0300

    change socket url

--- src/pages/Messenger/Messenger.jsx
+++ src/pages/Messenger/Messenger.jsx
@@ -19,7 +19,7 @@ function Messenger() {
   const socket = useRef();
   const [conversation, setConversation] = useState(null);
   useEffect(() => {
-    socket.current = io("https://young-basin-78153.herokuapp.com/");
+    socket.current = io("https://immense-shore-19135.herokuapp.com/");
     socket.current.emit("addUser", userId);
     socket.current.on("getMessage", (message) => {
       console.log(message);
