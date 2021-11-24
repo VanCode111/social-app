ca1c7dff3b07734a480a14fab26ff2d9a6e04ba0
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-11-24 03:45:27 +0300

    fix bcrypt

--- services/userService.js
+++ services/userService.js
@@ -11,7 +11,7 @@ class UserService {
     if (!user) {
       throw new Error("Неверный логин или пароль");
     }
-    const comparedPasswords = await bcrypt.compare(user.password, password);
+    const comparedPasswords = bcrypt.compareSync(password, user.password);
     if (!comparedPasswords) {
       throw new Error("Неверный логин или пароль");
     }
